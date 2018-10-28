25cb295213d9876c2ea28e38f4b076f467c2c031
zkf101 <42493830+zkf101@users.noreply.github.com>
2018-10-28 20:35:34 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const TenmaGabriel = new Discord.Client();
+const client = new Discord.Client();
 const exec = require("child_process").exec
 
 exec('JMusicBot-0.1.3.jar').unref()
